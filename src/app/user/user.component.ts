243d18e208f0485a19a9d5d68b0e0bc01362c943
VeederLicht <veederlicht@gmail.com>
2024-09-16 11:56:00 +0200

    tm 2.27

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+
+let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,6 +11,16 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+export class UserComponent {
+    selectedUser : any = DUMMY_USERS[randomIndex];
+
+    get imagePath(){
+        return 'assets/user-images/' + this.selectedUser.avatar
+    }
+
+    onSelectUser(){
+        randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+        this.selectedUser = DUMMY_USERS[randomIndex];
+    }
 }
